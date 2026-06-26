03c730c09cf1fa92f32455093dfde5dc840f4880
William Hou <williamhou38@yahoo.com>
2026-06-26 13:55:41 -0500

    new changes

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -19,14 +19,12 @@ export class TasksComponent {
 
   constructor(private tasksService: TasksService) {}
 
-  tasks: Task[] = [];
-
   get selectedUserTasks() {
     return this.tasksService.getUserTasks(this.userId);
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.tasksService.removeTask(id);
   }
 
   onStartAddTask() {
