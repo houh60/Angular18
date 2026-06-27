fcb352a2a2a00abed1f8feb9bb209335d8efed49
William Hou <williamhou38@yahoo.com>
2026-06-27 01:52:54 -0500

    new changes

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -31,15 +31,28 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId == userId);
   }
 
   addTask(task: Task) {
     this.tasks.push(task);
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
